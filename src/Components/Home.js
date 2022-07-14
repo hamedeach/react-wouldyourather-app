9fea2f6c5240f10a34e02c5cb2a9a84844c7e229
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-07-14 15:58:22 +0200

    polls list filtering by authed user info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,30 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./Navbar";
 import UserInfo from "./UserInfo";
 import PollList from './PollList.js';
 
 
-export default function Home() {
-    return (
-        <div className="App">
-            <div>
-                <div className="homebar-container">
-                    <div className="navbar-container">
-                        <Navbar />
-                    </div>
-                    <div className="userinfo-container">
-                        <UserInfo />
-                    </div>
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            poll_tab_id: '0',
+        }
 
+    }
+
+    changePollsTab=(tabid)=> {
+
+        this.setState(() => ({
+            poll_tab_id: tabid
+        }))
+
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <div className="homebar-container">
+                        <div className="navbar-container">
+                            <Navbar />
+                        </div>
+                        <div className="userinfo-container">
+                            <UserInfo />
+                        </div>
+
+                    </div>
+                    <hr></hr>
                 </div>
-                <hr></hr>
+
+                <PollList PollTabID={this.state.poll_tab_id} ChangePollsTab={this.changePollsTab} />
+
             </div>
-
-            <PollList />
-
-        </div>
-    )
+        )
+    }
 }
+export default Home;
 
 
 
--- src/Components/PollList.js
+++ src/Components/PollList.js
@@ -4,14 +4,35 @@ import { connect } from 'react-redux';
 
 class PollList extends Component {
 
+    changetab = (e) => {
+        switch (e.target.id) {
+            case '1':
+                {
+
+                    document.getElementById("0").setAttribute("class", "poll-tab-op");
+                    document.getElementById("1").setAttribute("class", "active-poll-tab-op");
+                    break;
+
+                }
+            default:
+                {
+                    document.getElementById("1").setAttribute("class", "poll-tab-op");
+                    document.getElementById("0").setAttribute("class", "active-poll-tab-op");
+                    break;
+                }
+
+        }
+        this.props.ChangePollsTab(e.target.id);
+    }
+
     render() {
 
         const { authedUser, users, questions } = this.props;
         return (
             <div className="polls-div">
                 <div className="poll-tab">
-                    <h2 className="active-poll-tab-op">not Answer Questions</h2>
-                    <h2 className="poll-tab-op">Answer Questions</h2>
+                    <h2 id={"0"} onClick={this.changetab} className="active-poll-tab-op"> Not-answered Questions</h2>
+                    <h2 id={"1"} onClick={this.changetab} className="poll-tab-op">Answered Questions</h2>
                 </div>
                 <ul>
                     {
@@ -29,15 +50,39 @@ class PollList extends Component {
 }
 
 
-function mapStateToProps({ authedUser, users, questions }) {
+function mapStateToProps({ authedUser, users, questions }, PollProps) {
+
+    const all_questions_arr = Object.keys(questions).map(id => {
+        return questions[id]
+    });
+
+    const authedUserObj = Object.keys(users).map(id => {
+        return users[id]
+    }).find(u => u.id === authedUser);
+   // console.log(authedUserObj);
+
+    const autheduser_answers = Object.keys(authedUserObj.answers);
+    //console.log(autheduser_answers)
+    let filtered_questions = [];
+    filtered_questions = [];
+
+    switch (PollProps.PollTabID) {
+        case '1': // answer
+            filtered_questions = all_questions_arr.filter((q) => (autheduser_answers.includes(q.id)));
+            break;
+        default:
+            filtered_questions = all_questions_arr.filter((q) => !(autheduser_answers.includes(q.id)));
+            break;
+
+    }
+    //console.log(filtered_questions);
+
     return {
         authedUser,
         users: Object.keys(users).map(id => {
             return users[id]
         }),
-        questions: Object.keys(questions).map(id => {
-            return questions[id]
-        }).sort((a, b) => b.timestamp - a.timestamp),
+        questions: filtered_questions.sort((a, b) => b.timestamp - a.timestamp),
     }
 
 }
